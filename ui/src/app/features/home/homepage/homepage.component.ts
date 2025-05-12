9d23fb4b0553adabc1c68c30ebbb554fc4090053
Alex Bilson <acbilson@gmail.com>
2025-05-12 14:50:11 -0600

    adds type of people nav

--- ui/src/app/features/home/homepage/homepage.component.ts
+++ ui/src/app/features/home/homepage/homepage.component.ts
@@ -3,10 +3,11 @@ import { TextService } from '../../../core/api/v1/api/api';
 import { FadeInDirective } from '../directives/fade-in.directive';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-homepage',
-  imports: [FadeInDirective, MatButtonModule, RouterLink],
+  imports: [FadeInDirective, MatButtonModule, RouterLink, MatDividerModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
